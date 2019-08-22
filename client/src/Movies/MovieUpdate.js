370465e8f7511f57d4b3e68945399add5d493963
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-22 11:07:28 -0400

    Created update form with put request

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -1,11 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+import { ButtonContainer } from "../styled-components/Button";
+import axios from "axios";
 
-const MovieUpdate = () => {
+const MovieUpdate = props => {
+  const [update, setUpdate] = useState({
+    title: "",
+    director: "",
+    metascore: "",
+    stars: ""
+  });
+  const changeHandler = event => {
+    event.preventDefault();
+    setUpdate({ ...update, [event.target.name]: event.target.value });
+  };
+
+  const updateMovie = e => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5000/api/movies/${props.match.params.id}`, update)
+      .catch(err => console.log(err.response));
+    props.history.push(`/`);
+  };
+
   return (
-    <div>
-      <h1>Update Movie</h1>
+    <div className="updateModal">
+      <form className="update-modal" onSubmit={event => updateMovie(event)}>
+        <h1 className="updateTitle">Update Movie !</h1>
+        <p className="updateInputs">
+          <label>
+            Title:
+            <input
+              className="input-modal"
+              type="text"
+              name="title"
+              onChange={changeHandler}
+              value={update.title}
+            />
+          </label>
+        </p>
+
+        <p className="updateInputs">
+          <label>
+            Director:
+            <input
+              className="input-modal"
+              type="director"
+              name="director"
+              onChange={changeHandler}
+              value={update.director}
+            />
+          </label>
+        </p>
+
+        <p className="updateInputs">
+          <label>
+            Metascore:
+            <input
+              className="input-modal"
+              type="metascore"
+              name="metascore"
+              onChange={changeHandler}
+              value={update.metascore}
+            />
+          </label>
+        </p>
+
+        <p className="updateInputs">
+          <label>
+            Stars:
+            <input
+              className="input-modal"
+              type="stars"
+              name="stars"
+              onChange={changeHandler}
+              value={update.stars}
+            />
+          </label>
+        </p>
+
+        <ButtonContainer
+          className="button-modal"
+          onClick={event => updateMovie(event)}
+        >
+          Update!
+        </ButtonContainer>
+      </form>
     </div>
   );
 };
 
-export default MovieUpdate;
+export default withRouter(MovieUpdate);
